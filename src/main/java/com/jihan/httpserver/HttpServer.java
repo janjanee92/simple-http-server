eac523e0732e5691a96571745d29484f5ddafdcb
rowanlee92 <watrv41@gmail.com>
2020-12-01 00:08:38 +0900

    Update: Multiple Connections using Threads

--- src/main/java/com/jihan/httpserver/HttpServer.java
+++ src/main/java/com/jihan/httpserver/HttpServer.java
@@ -2,51 +2,35 @@ package com.jihan.httpserver;
 
 import com.jihan.httpserver.config.Configuration;
 import com.jihan.httpserver.config.ConfigurationManager;
+import com.jihan.httpserver.core.ServerListenerThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class HttpServer {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
+
     public static void main(String[] args) {
-        System.out.println("Server starting...");
+
+        LOGGER.info("Server starting...");
 
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
 
-        System.out.println("Using Port:" + conf.getPort());
-        System.out.println("Using WebRoot:" +conf.getWebroot());
+        LOGGER.info("Using Port:" + conf.getPort());
+        LOGGER.info("Using WebRoot:" +conf.getWebroot());
 
+        ServerListenerThread serverListenerThread = null;
         try {
-            ServerSocket serverSocket = new ServerSocket(conf.getPort());
-            Socket socket = serverSocket.accept();
-
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my Simple Java HTTP Server</h1></body></html>";
-
-            final String CRLF = "\n\r"; // 13, 10
-
-            String response =
-                    "HTTP/1.1 200 OK" + CRLF +  // Status Line: HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
-                    "Content-Length:" + html.getBytes().length + CRLF + // HEADER
-                            CRLF +
-                            html +
-                            CRLF + CRLF;
-
-            outputStream.write(response.getBytes());
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            serverSocket.close();
-
+            serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
         } catch (IOException e) {
-            e.printStackTrace();
+            // TODO handle later
         }
+
+
 
     }
 }
