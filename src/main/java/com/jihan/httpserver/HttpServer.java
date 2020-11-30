d7b5e866349e68a885d89acf730ea929985fbc0c
rowanlee92 <watrv41@gmail.com>
2020-11-30 23:12:16 +0900

    Add: server sockets

--- src/main/java/com/jihan/httpserver/HttpServer.java
+++ src/main/java/com/jihan/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package com.jihan.httpserver;
 import com.jihan.httpserver.config.Configuration;
 import com.jihan.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
     public static void main(String[] args) {
         System.out.println("Server starting...");
@@ -12,6 +18,35 @@ public class HttpServer {
 
         System.out.println("Using Port:" + conf.getPort());
         System.out.println("Using WebRoot:" +conf.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my Simple Java HTTP Server</h1></body></html>";
+
+            final String CRLF = "\n\r"; // 13, 10
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF +  // Status Line: HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                    "Content-Length:" + html.getBytes().length + CRLF + // HEADER
+                            CRLF +
+                            html +
+                            CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
